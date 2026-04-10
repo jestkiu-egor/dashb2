f98bd7d16af3c4173868aba6be83c493293dbe11
KortezDmitry <kortez.pc@gmail.com>
2026-04-10 15:17:37 +0300

    Удаление секретных данных и обновление .gitignore
    Проблема: Файл 1. GEMINI_AUTH.bat периодически попадал в коммиты из-за отсутствия в .gitignore и сброса веток.
    
    Как решили:
    1. Файл 1. GEMINI_AUTH.bat окончательно добавлен в .gitignore.
    2. Файл удален из отслеживания Git (git rm --cached), при этом физически он остался на диске пользователя.
    3. Изменения зафиксированы в текущей ветке kortez_vetka.

--- 1. GEMINI_AUTH.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set HTTP_PROXY=http://UZtsd1:h4fWKh@161.115.231.113:9149
-set HTTPS_PROXY=http://UZtsd1:h4fWKh@161.115.231.113:9149
-set ALL_PROXY=http://UZtsd1:h4fWKh@161.115.231.113:9149
-set NO_PROXY=localhost,127.0.0.1
-gemini
-pause
